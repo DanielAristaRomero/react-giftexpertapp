6811f798a95fb30a489b074dfa575e27a3681c08
Daniel Arista <arista.dan18@gmail.com>
2020-09-13 21:09:49 -0500

    docs agregado

---
+++ docs/precache-manifest.d43be2f77563d4c2ccd110af06ba00af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f807246bc6e97b771fa4224dd71298cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eaff5c8bdb92094f974",
+    "url": "/static/css/main.d706d748.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eaff5c8bdb92094f974",
+    "url": "/static/js/main.8a705f08.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
